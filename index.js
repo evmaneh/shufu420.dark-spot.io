808f699c5e6879c9fbb194ff7dab1d3a47c1cbfd
shufu420 <122318862+shufu420@users.noreply.github.com>
2023-01-13 07:38:02 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+async function getProxy(options, callback) {
+  if (typeof options == 'function') {callback = options; options = {}};
+  
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.getRegistrations().then(a=>a.forEach(e=>fetch(e.active.scriptURL).then(e=>e.text()).then(e=>e.includes('croxyproxy')?callback(true):null)))
+  }
+  
+  if (window.$Rhodium||window.__uv||window.alloyLocation||window.__get$Loc||window._womginx_fetch) {
+    callback(true)
+  }
+  
+  return callback(false);
+}
